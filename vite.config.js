5dbb059ad38d59e82175990513ef498ec677a849
atamaniuksofiia <sofiia.atamaniuk@edvantis.com>
2025-04-11 20:58:03 +0300

    � Fix GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-   base: '/test-tabs-1/',
+export default defineConfig(({ mode }) => ({
+  base:  mode === 'production' ? '/test-tabs-1/' : '/',
   plugins: [react()],
-   build: {
+  build: {
     sourcemap: true,
   },
-})
+}));
